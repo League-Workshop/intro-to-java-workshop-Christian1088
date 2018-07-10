c84c237bce8833339a85c6080bae7063420a15b2
Christian1088 <league@WTS20.attlocal.net>
2018-07-10 11:59:31 -0700

    Robots
    We drew shapes using a robot.

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -9,6 +9,19 @@ public class MyFirstJavaProgram {
 		
 		// START HERE
 	
+		Robot r2d2=new Robot();
+		r2d2.penDown();
+		r2d2.setPenColor(Color.DARK_GRAY);
+		r2d2.setSpeed(200);
+		
+	    for(int i=0;i<3;i++) {
+			r2d2.move(200);
+			r2d2.turn(120);
+			
+			
+			
+		}
+		in
 		
 		
 	}
