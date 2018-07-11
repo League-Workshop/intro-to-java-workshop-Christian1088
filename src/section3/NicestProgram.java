5699eb4df32f5fba537c1db937aba86e9e17c82c
aryanmshah <league@WTS20.attlocal.net>
2018-07-11 11:57:50 -0700

    Day 3

---
+++ src/section3/NicestProgram.java
@@ -0,0 +1,13 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class NicestProgram {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "I like your shoes");
+		JOptionPane.showMessageDialog(null,"cool shirt" );
+		JOptionPane.showMessageDialog(null, "cool socks");
+		
+	}
+
+}
