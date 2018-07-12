bd5d256dd21c2b0f5f1a03d448b438cc9661ac66
aryanmshah <league@WTS20.attlocal.net>
2018-07-12 11:58:00 -0700

    Day 4

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,30 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score=0;
 		// 2.  Ask the user a question 
-		
+		String answer=JOptionPane.showInputDialog("How many Super Bowls have the Broncos won?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(answer.equals("three")) {
 		// 4.  if the user's answer is correct
-		
+		score++;
 		// -- add one to their score 
-		
+		JOptionPane.showMessageDialog(null, "you are correct");
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		JOptionPane.showInputDialog("Are the Broncos a football team");
 		
+		if(answer.equals("yes")) {
+			score++;
+			JOptionPane.showMessageDialog(null, "you are correct");
+		}
+		}
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		JOptionPane.showInputDialog(null, "your score is");
 	}
 }
---
+++ src/section4/Terminator Cat/cat/cat.pde
@@ -0,0 +1,13 @@
+PImage catPic;
+
+void setup(){
+size(600,600); 
+catPic=loadImage("cat.jpg");
+catPic.resize(800,600);
+background(catPic);
+
+}
+
+void draw(){
+  
+}
